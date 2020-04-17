7282a43a6d01d28b02da43c36146d72cc3d91f03
Vivek Kumar <vivek.kumar@dnata.com>
2020-04-17 21:29:00 +0400

    indentation fix

--- src/App.js
+++ src/App.js
@@ -4,45 +4,26 @@ import './App.css';
 
 class App extends Component{
  
-  // state = {
-  //   count:0
-  // }
-  
-  // onKeyUp =()=>{
-  //   this.setState({
-  //     count:this.state.count+1
-  //   })
-  // }
-
-  // onKeyDown =()=>{
-  //   this.setState({
-  //     count:this.state.count-1
-  //   })
-  // }
-
   render(){
-    console.log("FDFDFVivek",JSON.stringify(this.props.onKeyUp))
-  return (
-    <div className="App">
-      <h1>Count: {this.props.someValue}</h1>
-      <button onClick={this.props.onKeyUp}>OnKeyUp</button>
-      <br></br>
-      <br></br>
-      <button onClick={this.props.onKeyDown}>OnKeyDown</button>
-    </div>
-
-  );
+    return (
+      <div className="App">
+        <h1>Count: {this.props.someValue}</h1>
+        <button onClick={this.props.onKeyUp}>OnKeyUp</button>
+        <br></br>
+        <br></br>
+        <button onClick={this.props.onKeyDown}>OnKeyDown</button>
+      </div>
+    );
   }
 }
 
 const mapStateToProps= (state) =>{
-  console.log("VCVCVC",JSON.stringify(state.count));
   return({
     someValue:state.count
   })
 }
 
- const mapDispatchToProps = (dispatch)=>{
+const mapDispatchToProps = (dispatch)=>{
    return(
    {
    onKeyUp:()=>dispatch({type:"OnKeyUp"}),
